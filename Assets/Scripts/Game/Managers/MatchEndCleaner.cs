d94dd7abf7dbbdff6b6a753ceb8bd6e9f49195c4
Kristupas Stumbrys <kristupas.stumbrys@gmail.com>
2020-02-02 14:44:31 +0200

    Cleanup cards when starting new match

---
+++ Assets/Scripts/Game/Managers/MatchEndCleaner.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class MatchEndCleaner : MonoBehaviour
+{
+    void Start()
+    {
+        EventCoordinator.StartListening(EventName.System.MatchStarted(), ClearCards);
+        EventCoordinator.StartListening(EventName.System.EndGame(), ClearCards);
+    }
+
+    void OnDestroy()
+    {
+        EventCoordinator.StopListening(EventName.System.MatchStarted(), ClearCards);
+        EventCoordinator.StopListening(EventName.System.EndGame(), ClearCards);
+    }
+
+    void ClearCards(GameMessage msg)
+    {
+        foreach (Transform child in transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+    }
+}
